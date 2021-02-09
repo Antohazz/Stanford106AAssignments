7f1e29394330243cf7bec05a19dd71c7575419c2
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-08 22:47:22 -0500

    Completed

--- src/week1/KarelDefendsDemocracy.java
+++ src/week1/KarelDefendsDemocracy.java
@@ -4,10 +4,42 @@ import stanford.karel.*;
 public class KarelDefendsDemocracy extends SuperKarel {
 
 	public void run() {
-		move();
-	move();
-	move();
+
+		cleanBallot();
+		
 	}
 	
+	private void doCleaning() {
+		turnLeft();
+		pickEmAll();
+		pickEmAll();
+		move();
+		turnRight();
+	}
+	
+	private void pickEmAll() {
+		while (frontIsClear()) {
+			move();
+		}
+		while (beepersPresent()) {
+			pickBeeper();
+		}
+		turnAround();
+	}
+	
+	private void cleanBallot() {
+		while (frontIsClear()) {
+			move();
+			if (noBeepersPresent()) {
+				doCleaning();
+				move();
+			}else{
+				move();
+				cleanBallot();
+			}
+			
+		}
+
+	}
 		
 }

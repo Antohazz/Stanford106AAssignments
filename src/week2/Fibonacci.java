dad7c4bc55538ab129ed3e0be666bf7a4a14d1d3
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-11 11:38:46 -0500

    Update Fibonacci.java
    Completed

--- src/week2/Fibonacci.java
+++ src/week2/Fibonacci.java
@@ -4,8 +4,22 @@ import acm.program.*;
 
 public class Fibonacci extends ConsoleProgram {
 
+	private static final int MAX_VALUE = 10000;
 	
 	public void run() {
 
+		println("Lady and Gentlemens,");
+		println("The Fibonacci Sequence!");
+		
+		int fib = 1;
+		println("0");
+		println("1");
+		do {println(fib);
+			fib+=fib;
+			if (fib > MAX_VALUE) {
+					break;
+			}
+		}
+		while (fib < MAX_VALUE);
 	}
 }

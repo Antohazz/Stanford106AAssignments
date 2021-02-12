863a6117e671d48395486c26024513b92ce2b6a9
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-12 18:55:20 -0500

    Completed

--- src/week2/RandomCircles.java
+++ src/week2/RandomCircles.java
@@ -1,7 +1,24 @@
+
 package week2;
 
+
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class RandomCircles extends GraphicsProgram {
 
+	public void run () {
+			for (int i = 0; i<10; i++) {
+				int radius= rgen.nextInt(5, 50);
+				int x = rgen.nextInt(0, (getWidth()-2*radius));
+				int y = rgen.nextInt(0, (getHeight()-2*radius));
+				GOval myCircle = new GOval(x,y,radius*2,radius*2);
+				myCircle.setFilled(true);
+				myCircle.setColor(rgen.nextColor());
+// white circle not seen
+				add(myCircle);
+			}
+	}
+RandomGenerator rgen = RandomGenerator.getInstance();
 }

d7a6f8ad8ecb3b5503bdce25c2c1c75cc4b398f9
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-12 20:06:51 -0500

    Refined

--- src/week2/RandomCircles.java
+++ src/week2/RandomCircles.java
@@ -11,18 +11,21 @@ import acm.util.RandomGenerator;
 public class RandomCircles extends GraphicsProgram {
 
 	public void run () {
-			for (int i = 0; i<10; i++) {
+			for (int i = 0; i < 10; i++) {
 				int radius= rgen.nextInt(5, 50);
-				int x = rgen.nextInt(0, (getWidth()-2*radius));
-				int y = rgen.nextInt(0, (getHeight()-2*radius));
-				GOval myCircle = new GOval(x,y,radius*2,radius*2);
+				int x = rgen.nextInt(0, (getWidth() - 2*radius));
+				int y = rgen.nextInt(0, (getHeight() - 2*radius));
+				
+				GOval myCircle = new GOval(x, y, radius*2, radius*2);
 				myCircle.setFilled(true);
 				myCircle.setColor(rgen.nextColor());
+				
 				// color white circle with new random
 				while (myCircle.getColor() == Color.WHITE) {
 					myCircle.setColor(rgen.nextColor());
 				}
-				add(myCircle);
+				
+			add(myCircle);
 			}
 	}
 RandomGenerator rgen = RandomGenerator.getInstance();

b0deeb609893a6e69b0911710e426b2caa43eb1e
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-14 23:17:59 -0500

    Paddle added, resized

--- src/week2/Breakout.java
+++ src/week2/Breakout.java
@@ -21,19 +21,19 @@ import javax.swing.JPopupMenu.Separator;
 public class Breakout extends GraphicsProgram {
 
 /** Width and height of application window in pixels */
-	public static final int APPLICATION_WIDTH = 400;
-	public static final int APPLICATION_HEIGHT = 600;
+	public static final int APPLICATION_WIDTH = 800;
+	public static final int APPLICATION_HEIGHT = 1200;
 
 /** Dimensions of game board (usually the same) */
 	private static final int WIDTH = APPLICATION_WIDTH;
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
 /** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 60;
-	private static final int PADDLE_HEIGHT = 10;
+	private static final int PADDLE_WIDTH = 120;
+	private static final int PADDLE_HEIGHT = 20;
 
 /** Offset of the paddle up from the bottom */
-	private static final int PADDLE_Y_OFFSET = 30;
+	private static final int PADDLE_Y_OFFSET = 120;
 
 /** Number of bricks per row */
 	private static final int NBRICKS_PER_ROW = 10;
@@ -42,20 +42,20 @@ public class Breakout extends GraphicsProgram {
 	private static final int NBRICK_ROWS = 10;
 
 /** Separation between bricks */
-	private static final int BRICK_SEP = 4;
+	private static final int BRICK_SEP = 8;
 
 /** Width of a brick */
 	private static final int BRICK_WIDTH =
 	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
 /** Height of a brick */
-	private static final int BRICK_HEIGHT = 8;
+	private static final int BRICK_HEIGHT = 16;
 
 /** Radius of the ball in pixels */
-	private static final int BALL_RADIUS = 10;
+	private static final int BALL_RADIUS = 20;
 
 /** Offset of the top brick row from the top */
-	private static final int BRICK_Y_OFFSET = 70;
+	private static final int BRICK_Y_OFFSET = 140;
 
 /** Number of turns */
 	private static final int NTURNS = 3;
@@ -65,18 +65,20 @@ public class Breakout extends GraphicsProgram {
 	public void run() {
 		/* You fill this in, along with any subsidiary methods */
 		
-		setupWall();
+		setup();
+		play();
 		
 	}
 	
-	private void setupWall() {
+	private void setup() {
 		createWallOfBricks();
-		
+		createPaddle();
+		addMouseListeners();
 	}
 	
-	
+		
 	private void createWallOfBricks() {
-		int y = 10 * BRICK_HEIGHT;
+		int y = BRICK_Y_OFFSET;
 		int r = 255;
 		int g = 50;
 		int b = 0;
@@ -99,15 +101,62 @@ public class Breakout extends GraphicsProgram {
 	private void createRowOfBricks(int y, Color currentColor) {
 		int x = BRICK_SEP/2;
 		for (int i = 0; i < NBRICKS_PER_ROW; i++) {
-		GRect brick = new GRect (x,y, BRICK_WIDTH, BRICK_HEIGHT);
+		brick = new GRect (x,y, BRICK_WIDTH, BRICK_HEIGHT);
 		x = x + BRICK_WIDTH + BRICK_SEP;
 		brick.setFilled(true);
 		brick.setColor(currentColor);
 		add(brick);
 		}
 	}
+	
+	
+	public void createPaddle() {
+		int x = WIDTH / 2 - PADDLE_WIDTH / 2;
+		paddleY = (HEIGHT - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+		paddle = new GRect (x, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setColor(Color.black);
+		add(paddle);
+	}
+
+	public void mouseMoved(MouseEvent e) {
+        int x = e.getX() - PADDLE_WIDTH / 2;
+        paddle.setLocation(x,paddleY);
+        if (paddle.getX() <= 0) {
+        	paddle.setLocation(0, paddleY);
+        }
+        if (paddle.getX() + PADDLE_WIDTH >= WIDTH) {
+        	paddle.setLocation(WIDTH - PADDLE_WIDTH, paddleY);
+        }
+		
+	}
+	
+	private void play() {
+		createBall();
+		while(!gameOver()) {
+			
+			
+			
+			
+		}
+	}
 
 	
+	private void createBall() {
+		ball = new GOval(WIDTH / 2 - BALL_RADIUS, HEIGHT / 2 + BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2);
+		ball.setFilled(true);
+		ball.setColor(Color.black);
+		add(ball);
+	}
+	
+	private boolean gameOver() {
+		return (brick == null); 
+	}
+	
+	private GRect paddle;
 	private GRect brick;
+	private GOval ball;
+	private int paddleY;
+
 	
 }

ec687b73bfb2355a57e4e0d71e7646e79f301ac2
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-17 23:31:54 -0500

    Works

--- src/week3/AddCommasToNumberStrings.java
+++ src/week3/AddCommasToNumberStrings.java
@@ -6,6 +6,31 @@ public class AddCommasToNumberStrings extends ConsoleProgram {
 	
 	public void run() {
 
+		while(true) {
+		String digits = readLine("Enter a numeric string: ");
+		if (digits.length() == 0) {
+			break;
+		}
+		
+		println(addCommasToNumericString(digits));
+	 }
 	}
+	
+	private String addCommasToNumericString(String digits) {
+		int x = digits.length() % 3;
+		if (x == 0) {
+			x = 3;
+		}
+		
+		String sepDigits = digits.substring(0, x);
+		
+		for (int i = 0; i < digits.length()-x; i+=3) {
+			sepDigits = sepDigits + "," + digits.substring(x, x + 3);
+		}
+				
+		return sepDigits;
+		
+	}
+	
 	
 }

3d5c4d071a2aa0472db63d6a4192b1af20ebcba0
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-18 11:30:22 -0500

    Completed

--- src/week3/AddCommasToNumberStrings.java
+++ src/week3/AddCommasToNumberStrings.java
@@ -8,12 +8,12 @@ public class AddCommasToNumberStrings extends ConsoleProgram {
 
 		while(true) {
 		String digits = readLine("Enter a numeric string: ");
-		if (digits.length() == 0) {
-			break;
-		}
+			if (digits.length() == 0) {
+				break;
+			}
 		
 		println(addCommasToNumericString(digits));
-	 }
+		}
 	}
 	
 	private String addCommasToNumericString(String digits) {
@@ -24,10 +24,10 @@ public class AddCommasToNumberStrings extends ConsoleProgram {
 		
 		String sepDigits = digits.substring(0, x);
 		
-		for (int i = 0; i < digits.length()-x; i+=3) {
+		for (int i = 0; i < (digits.length() - x) ; i += 3) {
 			sepDigits = sepDigits + "," + digits.substring(x, x + 3);
 		}
-				
+			
 		return sepDigits;
 		
 	}
--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -5,7 +5,17 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		
+		println(removeAllOccurrences("tThis is a test,t tveryt bigt tttest", 't'));
+		
 	}
 	
+	public String removeAllOccurrences(String str, char ch) {
+		int n = str.indexOf(ch);
+		while(n != -1) {
+			str = str.substring(0, n) + str.substring(n + 1);
+			n = str.indexOf(ch);
+		}
+		return str;
+	}
 }

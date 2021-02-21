d6a6a947aa315318cc5f58a3b50b426367f5b572
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-21 00:09:22 -0500

    Second way added

--- src/week2/Fibonacci.java
+++ src/week2/Fibonacci.java
@@ -6,20 +6,43 @@ public class Fibonacci extends ConsoleProgram {
 
 	private static final int MAX_VALUE = 10000;
 
+	
 	public void run() {
 
-		println("Lady and Gentlemens,");
+		println("Ladies and Gentlemen,");
 		println("The Fibonacci Sequence!");
 		
-		int fib0 = 0;
+		int one = 0;
+		int two = 1;
+		int thr = one + two;
 		
-		for (int fib1 = 1; fib1 < MAX_VALUE; fib1 += fib0 ) {
-			println(fib0);
-			println(fib1);
-			fib0 += fib1; 
+		while (true){
+
+			if (one <= 	MAX_VALUE)
+				println(one);
+			if (two <= MAX_VALUE)
+				println(two);
+			if (thr <= MAX_VALUE)
+				println(thr);
+			else break;
+			
+			one = two + thr;
+			two = thr + one;
+			thr = one + two;
 		}
-		if (fib0 < MAX_VALUE) {
-			println(fib0);
-		}
+		
+		
+//		int fib0 = 0;
+//		
+//		for (int fib1 = 1; fib1 < MAX_VALUE; fib1 += fib0 ) {
+//			println(fib0);
+//			println(fib1);
+//			fib0 += fib1; 
+//		}
+//		if (fib0 < MAX_VALUE) {
+//			println(fib0);
+//		}
 	}
+	
+	
 }

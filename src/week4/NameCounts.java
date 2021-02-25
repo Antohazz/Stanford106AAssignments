d128c5c60228766b2b51cadd4b94a028cae61714
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-25 18:18:06 -0500

    Completed

--- src/week4/NameCounts.java
+++ src/week4/NameCounts.java
@@ -1,11 +1,47 @@
 package week4;
 
 import acm.program.ConsoleProgram;
+import acmx.export.java.util.ArrayList;
+import java.util.*;
 
 public class NameCounts extends ConsoleProgram {
 	
+	private ArrayList names = new ArrayList();
+	
 	public void run() {
-
+		
+		
+		
+		while (true) {
+			String line = readLine("Enter name: ");
+			if (line.length() == 0) {
+				printNames(names);
+				break;
+			}
+			
+			addsToArr(line);
+			
+											
+		}
+		
+		
+	}//run
+	
+	private void addsToArr(String line) {
+		if (!names.contains(line)){
+			names.add(line);
+		}
 	}
 	
-}
+	private void printNames(ArrayList names) {
+		
+		println("Uniqe list contains:");
+		
+		for (int i = 0; i < names.size(); i++) {
+			println(names.get(i));
+		}
+		
+		
+	}
+	
+}//class

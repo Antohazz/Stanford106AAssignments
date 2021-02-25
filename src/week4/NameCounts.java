3fc16b4789d5a1fe401c75f05fef0d8b0befff29
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-25 18:24:04 -0500

    Completed

--- src/week4/NameCounts.java
+++ src/week4/NameCounts.java
@@ -10,37 +10,7 @@ public class NameCounts extends ConsoleProgram {
 	
 	public void run() {
 		
-		
-		
-		while (true) {
-			String line = readLine("Enter name: ");
-			if (line.length() == 0) {
-				printNames(names);
-				break;
-			}
-			
-			addsToArr(line);
-			
-											
-		}
-		
-		
-	}//run
 	
-	private void addsToArr(String line) {
-		if (!names.contains(line)){
-			names.add(line);
-		}
-	}
-	
-	private void printNames(ArrayList names) {
-		
-		println("Uniqe list contains:");
-		
-		for (int i = 0; i < names.size(); i++) {
-			println(names.get(i));
-		}
-		
 		
 	}
 	
--- src/week4/UniqueNames.java
+++ src/week4/UniqueNames.java
@@ -1,11 +1,46 @@
 package week4;
 
 import acm.program.ConsoleProgram;
+import acm.program.ConsoleProgram;
+import acmx.export.java.util.ArrayList;
+import java.util.*;
 
 public class UniqueNames extends ConsoleProgram {
+private ArrayList names = new ArrayList();
 	
 	public void run() {
-
+				
+		
+		while (true) {
+			String line = readLine("Enter name: ");
+			if (line.length() == 0) {
+				printNames(names);
+				break;
+			}
+			
+			addsToArr(line);
+			
+											
+		}
+		
+		
+	}//run
+	
+	private void addsToArr(String line) {
+		if (!names.contains(line)){
+			names.add(line);
+		}
 	}
 	
-}
+	private void printNames(ArrayList names) {
+		
+		println("Uniqe list contains:");
+		
+		for (int i = 0; i < names.size(); i++) {
+			println(names.get(i));
+		}
+		
+		
+	}
+	
+}//class

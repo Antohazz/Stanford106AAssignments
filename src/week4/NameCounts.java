b9aad807dbbd9f6dd05ab3a7f90ed281e7084c45
Anton Ivanov <antoniv.eb@gmail.com>
2021-02-26 22:34:05 -0500

    Completed

--- src/week4/NameCounts.java
+++ src/week4/NameCounts.java
@@ -3,19 +3,42 @@ package week4;
 import acm.program.ConsoleProgram;
 import acmx.export.java.util.ArrayList;
 import java.util.*;
-
+import java.util.*; 
 public class NameCounts extends ConsoleProgram {
 	
-	private ArrayList names = new ArrayList();
-	
+private HashMap<String, Integer> names = new HashMap<String, Integer>();
+
 	public void run() {
-		
+
+		while (true) {
+			String line = readLine("Enter name: ");
+			if (line.length() == 0) {
+				printNames(names);
+				break;
+			}
+			
+			if (names.containsKey(line)) {
+				int x = names.get(line);
+				x++;
+				names.put(line, x);
+			}else {
+				names.put(line, 1);
+			}
+			
+			
+		}//while
+				
+	}//run
 	
-		
-		
-		
-		
-		
+	
+	private void printNames(HashMap<String, Integer> names) {
+		Iterator <String> iter = names.keySet().iterator();
+		while (iter.hasNext()) {
+			String k = iter.next();
+			Integer number = (Integer) names.get(k);
+			println("Name "+ k +": "+ number);
+
+		}
 	}
 	
 }//class

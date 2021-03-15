f6cae0c83e1ea1e457df9ec1911497867e8844a6
Anton Ivanov <antoniv.eb@gmail.com>
2021-03-15 16:09:28 -0400

    Done

--- src/week6/ExpandableArray.java
+++ src/week6/ExpandableArray.java
@@ -6,6 +6,43 @@ public class ExpandableArray extends ConsoleProgram {
 	
 	public void run() {
 
+		EArray a = new EArray();
+		a.set(5, "555");
+		println(a.get(5));
+		println(a.get(6));
+	
+}
+	
+	public class EArray {
+
+		private Object[] arr;
+
+		public EArray() {
+			arr = new Object[0];
+		}
+
+		public void set(int index, Object value) {
+			if (index >= arr.length) {
+				Object[] temp = new Object[index+1];
+				
+				for (int i = 0; i < arr.length; i++) {
+					temp[i] = arr[i];
+				}
+				
+				arr = temp;
+			}
+			
+			arr[index] = value;
+		}
+
+
+		public Object get(int index) {
+			if (index >= arr.length) {
+				return null;
+			}
+			return arr[index];
+		}
+		
 	}
 	
 }
